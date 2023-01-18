778050a5262029194c7154e1957f754fd8e30940
Joseph Peter <josephvpeter7@gmail.com>
2023-01-18 10:07:51 -0500

    Error: configuring Terraform AWS Provider

--- terraform-aws/providers.tf
+++ terraform-aws/providers.tf
@@ -11,7 +11,9 @@ provider "aws" {
   region                = var.aws_region
   # aws_access_key_id     = var.aws_access_key_id
   # aws_secret_access_key = var.aws_secret_access_key
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
+  # access_key = var.aws_access_key_id
+  # secret_key = var.aws_secret_access_key
 }
+
+
 
