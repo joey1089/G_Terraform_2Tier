ab4a51089fbc318fa3eab99c3ad60acc3209ead6
Joseph Peter <josephvpeter7@gmail.com>
2023-01-18 20:10:45 -0500

    added local vpc cider block

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -1,8 +1,13 @@
 # ----root/main.tf ---
 
+# local cidr block value for VPC
+locals {
+  vpc_cidr = "10.10.0.0/16"
+}
+
 module "networking" {
   source        = "./networking"
-  vpc_cidr      = "10.10.0.0/16"
+  vpc_cidr      = local.vpc_cidr
   # public_cidrs  = ["10.10.2.0/24", "10.10.4.0/24"] 
   # put in for loop and in range give subnet range
   # private_cidrs = ["10.10.1.0/24", "10.10.3.0/24"]
@@ -10,6 +15,7 @@ module "networking" {
   #length(public_cidrs) - doesn'twork
   public_sn_count = 2
   max_subnets = 20
-  public_cidrs = [for i in range(2,255,2) : cidrsubnet("10.10.0.0/16", 8, i)]
-  private_cidrs = [for i in range(1,255,2) : cidrsubnet("10.10.0.0/16", 8, i)]
+  public_cidrs = [for i in range(2,255,2) : cidrsubnet(local.vpc_cidr, 8, i)]
+  private_cidrs = [for i in range(1,255,2) : cidrsubnet(local.vpc_cidr, 8, i)]
+  # private_cidrs = [for i in range(1,255,2) : cidrsubnet("10.10.0.0/16", 8, i)]
 }
