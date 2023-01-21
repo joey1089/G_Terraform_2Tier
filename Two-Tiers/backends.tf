ee056a43a7bf8b0fafd2ac9cdc83efa8ce8fa7b7
Joseph Peter <josephvpeter7@gmail.com>
2023-01-21 02:04:53 -0500

    retrying modular level

---
+++ Two-Tiers/backends.tf
@@ -0,0 +1,12 @@
+# --- remote/backends.tf ---
+
+# terraform {
+#   backend "remote" {
+#     hostname     = "app.terraform.io"
+#     organization = "MyCompanyHere"
+
+#     workspaces {
+#       name = "TwoTier-aws"
+#     }
+#   }
+# }
--- test_2tier/main.tf
+++ test_2tier/main.tf
@@ -13,22 +13,27 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, 0)
-  availability_zone = data.aws_availability_zones.all.names[0]
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 0)
+  availability_zone       = data.aws_availability_zones.all.names[0]
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private_web" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
   availability_zone = data.aws_availability_zones.all.names[1]
 }
 
 resource "aws_subnet" "private_rds" {
+  count  = 2
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
-  availability_zone = data.aws_availability_zones.all.names[2]
+  #   cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, 4)
+#   cidr_block    = [for i in range(1, 255, 2) : cidrsubnet("10.0.0.0/16", 8, i)]
+  availability_zone = data.aws_availability_zones.all.names[count.index]
+  tags = {
+    Name = "private-rds-subnet-${count.index + 3}"
+  }
 }
 
 
@@ -45,8 +50,16 @@ resource "aws_security_group" "bastion" {
 }
 
 resource "aws_security_group" "web" {
-  name   = "web"
-  vpc_id = aws_vpc.main.id
+  name        = "web"
+  description = "Security group for web servers"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 80
@@ -54,11 +67,19 @@ resource "aws_security_group" "web" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "rds" {
-  name   = "rds"
-  vpc_id = aws_vpc.main.id
+  name        = "rds"
+  description = "Security group for RDS instances"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 3306
@@ -66,15 +87,23 @@ resource "aws_security_group" "rds" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "web" {
-  count           = 2
-  ami             = "ami-0ff8a91507f77f867"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.web.name]
-  subnet_id       = aws_subnet.private_web[count.index].id
-  user_data       = <<EOF
+  count                  = 2
+  ami                    = "ami-0ff8a91507f77f867"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web.id]
+  #   subnet_id       = aws_subnet.private_web[count.index].id
+  subnet_id = element(aws_subnet.private_web[*].id, 0)
+  user_data = <<EOF
     #!/bin/bash
     sudo yum install -y nginx
     sudo service nginx start
@@ -89,22 +118,25 @@ resource "aws_instance" "web" {
 resource "aws_db_subnet_group" "rds" {
   name        = "private_rds_subnet_group"
   description = "Subnet group for private RDS instances"
-  subnet_ids  = [for subnet in aws_subnet.private_rds : subnet.id]
+  #   subnet_ids  = [aws_subnet.private_rds.*.id]
+  subnet_ids = values(zipmap(aws_subnet.private_rds[*].id, aws_subnet.private_rds[*].id))
 }
 
 
 resource "aws_db_instance" "rds" {
-  count                  = 1
-  engine                 = "mysql"
-  engine_version         = "5.7"
-  instance_class         = "db.t2.micro"
-  storage_type           = "gp3"
-  allocated_storage      = 20
-  db_name                = "mydb"
-  username               = "myuser"
-  password               = "mypassword"
-  vpc_security_group_ids = [aws_security_group.rds.id]
-  db_subnet_group_name   = aws_db_subnet_group.rds.name
-  publicly_accessible    = false
+  count                     = 1
+  engine                    = "mysql"
+  engine_version            = "5.7"
+  instance_class            = "db.t2.micro"
+  storage_type              = "gp3"
+  allocated_storage         = 20
+  db_name                   = "mydb"
+  username                  = "myuser"
+  password                  = "mypassword"
+  vpc_security_group_ids    = [aws_security_group.rds.id]
+  db_subnet_group_name      = aws_db_subnet_group.rds.name
+  publicly_accessible       = false
+  skip_final_snapshot       = false
+  final_snapshot_identifier = "mydb-final-snapshot"
 }
 
